f23b5843f28e000848c8c95f423c269ef6036f5d
nathanielpascual <nath_pascual@yahoo.com>
2018-10-21 12:50:36 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,13 +14,30 @@ var io = socketIO(server);
 io.on('connection',(socket) =>{
     console.log('New User connected');
 
+
+    socket.emit('newMessage',{
+        from : 'Admin',
+        text : 'Welcome to the chat app',
+        createdAt : new Date().getDate()
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from : 'Admin',
+        text : 'New user joined',
+        createdAt : new Date().getDate()
+    });
    socket.on('createMessage',(newMessage)=>{
        console.log('createMessage',newMessage);
-       io.emit('newMessage',{
-           from : newMessage.from,
-           text : newMessage.text,
-           createdAt : new Date().getTime()
-       })
+       //io.emit('newMessage',{
+       //    from : newMessage.from,
+       //    text : newMessage.text,
+      //     createdAt : new Date().getTime()
+      // })
+      socket.broadcast.emit('newMessage',{
+        from : newMessage.from,
+        text : newMessage.text,
+        createdAt : new Date().getTime()
+      });
     });
 
     socket.on('disconnect',()=>
